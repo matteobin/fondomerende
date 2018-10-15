d5156c7856f985b4602ac0b8a26e95428d84960e
matteobin <matteobin@protonmail.com>
2018-10-15 14:30:08 +0200

    Added JSON response to eat function.

--- commands.php
+++ commands.php
@@ -8,12 +8,22 @@
             $outflow_id = $row['outflow_id'];
             $totalPrice = $quantity*$row['price_per_snack'];
         }
-        $dbManager->runPreparedQuery('UPDATE crates SET quantity = quantity-? WHERE outflow_id=?', [$quantity, $outflow_id], 'ii');
-        $dbManager->runPreparedQuery('UPDATE snacks_stock SET quantity = quantity-? WHERE snack_id=?', [$quantity, $snackId], 'ii');
-        $dbManager->runPreparedQuery('UPDATE eaten SET quantity = quantity+? WHERE snack_id=?', [$quantity, $snackId], 'ii');
-        $dbManager->runPreparedQuery('UPDATE users_funds SET amount = amount-? WHERE user_id=?', [$totalPrice, $userId], 'si');
-        $dbManager->runPreparedQuery('INSERT INTO actions (user_id, command_id, snack_id, snack_quantity) VALUES (?, ?, ?, ?)', [$userId, 1, $snackId, $quantity], 'iiii');
+        if isset($outflow_id) {
+            $dbManager->runPreparedQuery('UPDATE crates SET quantity = quantity-? WHERE outflow_id=?', [$quantity, $outflow_id], 'ii');
+            $dbManager->runPreparedQuery('UPDATE snacks_stock SET quantity = quantity-? WHERE snack_id=?', [$quantity, $snackId], 'ii');
+            $dbManager->runPreparedQuery('UPDATE eaten SET quantity = quantity+? WHERE snack_id=?', [$quantity, $snackId], 'ii');
+            $dbManager->runPreparedQuery('UPDATE users_funds SET amount = amount-? WHERE user_id=?', [$totalPrice, $userId], 'si');
+            $dbManager->runPreparedQuery('INSERT INTO actions (user_id, command_id, snack_id, snack_quantity) VALUES (?, ?, ?, ?)', [$userId, 1, $snackId, $quantity], 'iiii');
+            $response = array('success'=>true, 'code'=>201);
+        } else {
+            $response = array('success'=>false, 'code'=>404, 'message'=>'No crates containing snack id '.$snackId.'.');
+        }
         $dbManager->endTransaction();
         $dbManager->delQueryRes();
+        return json_encode($response);
+    }
+    
+    function buy() {
+        
     }
 ?>
