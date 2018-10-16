e5a0f67053a67cfcb598921f2c46336ce8bf88f7
matteobin <matteobin@protonmail.com>
2018-10-16 11:10:00 +0200

    Implemnted DbManger statement error handling through Exception throw and added catch in eat command.

--- commands.php
+++ commands.php
@@ -1,25 +1,29 @@
 <?php
     function eat($userId, $snackId, $quantity) {
         global $dbManager;
-        
-        $dbManager->startTransaction();
-        $dbManager->runPreparedQuery('SELECT outflow_id, price_per_snack FROM crates WHERE snack_id=? AND quantity!=0 ORDER BY expiration ASC LIMIT 1', [$snackId], 'i');
-        while ($row = $dbManager->getQueryRes()->fetch_assoc()) {
-            $outflow_id = $row['outflow_id'];
-            $totalPrice = $quantity*$row['price_per_snack'];
+        try {
+            $dbManager->startTransaction();
+            $dbManager->runPreparedQuery('SELECT outflow_id, price_per_snack FROM crates WHERE snack_id=? AND quantity!=0 ORDER BY expiration ASC LIMIT 1', [$snackId], 'i');
+            while ($row = $dbManager->getQueryRes()->fetch_assoc()) {
+                $outflow_id = $row['outflow_id'];
+                $totalPrice = $quantity*$row['price_per_snack'];
+            }
+            if (isset($outflow_id)) {
+                $dbManager->runPreparedQuery('UPDATE crates SET quantity = quantity-? WHERE outflow_id=?', [$quantity, $outflow_id], 'ii');
+                $dbManager->runPreparedQuery('UPDATE snacks_stock SET quantity = quantity-? WHERE snack_id=?', [$quantity, $snackId], 'ii');
+                $dbManager->runPreparedQuery('UPDATE eaten SET quantity = quantity+? WHERE snack_id=?', [$quantity, $snackId], 'ii');
+                $dbManager->runPreparedQuery('UPDATE users_funds SET amount = amount-? WHERE user_id=?', [$totalPrice, $userId], 'si');
+                $dbManager->runPreparedQuery('INSERT INTO actions (user_id, command_id, snack_id, snack_quantity) VALUES (?, ?, ?, ?)', [$userId, 1, $snackId, $quantity], 'iiii');
+                $response = array('success'=>true, 'code'=>204);
+            } else {
+                $response = array('success'=>false, 'code'=>404, 'message'=>'No crates containing snack id '.$snackId.'.');
+            }
+            $dbManager->endTransaction();
+            $dbManager->delQueryRes();
         }
-        if (isset($outflow_id)) {
-            $dbManager->runPreparedQuery('UPDATE crates SET quantity = quantity-? WHERE outflow_id=?', [$quantity, $outflow_id], 'ii');
-            $dbManager->runPreparedQuery('UPDATE snacks_stock SET quantity = quantity-? WHERE snack_id=?', [$quantity, $snackId], 'ii');
-            $dbManager->runPreparedQuery('UPDATE eaten SET quantity = quantity+? WHERE snack_id=?', [$quantity, $snackId], 'ii');
-            $dbManager->runPreparedQuery('UPDATE users_funds SET amount = amount-? WHERE user_id=?', [$totalPrice, $userId], 'si');
-            $dbManager->runPreparedQuery('INSERT INTO actions (user_id, command_id, snack_id, snack_quantity) VALUES (?, ?, ?, ?)', [$userId, 1, $snackId, $quantity], 'iiii');
-            $response = array('success'=>true, 'code'=>204);
-        } else {
-            $response = array('success'=>false, 'code'=>404, 'message'=>'No crates containing snack id '.$snackId.'.');
+        catch (Exception $statementException) {
+            $response = array('success'=>false, 'code'=>500, 'message'=>$statementException->getMessage());
         }
-        $dbManager->endTransaction();
-        $dbManager->delQueryRes();
         return json_encode($response);
     }
     
