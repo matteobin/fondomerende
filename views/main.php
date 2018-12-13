a2d2c683bc540d59b3953915b9b4542f32518936
matteobin <matteobin@protonmail.com>
2018-12-13 18:34:20 +0100

    Kept writing main view. Really nice text there by the way.

--- views/main.php
+++ views/main.php
@@ -1,5 +1,8 @@
 <h1>Hello there!</h1>
 <p>Welcome to the wonderfully edible world of Fondo Merende.<br>Here's a list of tasty activities you can choose between, to start your journey in this sexy web-based office pantry:</p>
 <ul>
-    <li>Go to the bank to <a href="index.php?view="><strong>DEPOSIT</strong></a> some moolah.</li>
+    <li>Reach for the wallet to <a href="index.php?view=deposit"><strong>DEPOSIT</strong></a> some moolah for this very Just Cause.</li>
+    <li>Team up with peers and <a href="index.php?view=add-snack"><strong>ADD</strong></a> to the list all the junk food you've only been dreaming about.</li>
+    <li>Move your lazy butt, get out to <a href="index.php?view=buy"><strong>BUY</strong></a> the damn snacks.</li>
+    <li>Done? Now chill: open the fridge and <a href="index.php?view=eat"><strong>EAT</strong></a> them all.</li>
 </ul>
