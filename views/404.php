64239b25ba9da18220fe1f37322341175cf5970b
matteobin <matteobin@protonmail.com>
2019-01-25 14:15:53 +0100

    Fixed small navigation issue.

--- views/404.php
+++ views/404.php
@@ -2,5 +2,5 @@
     <h1 style="float:left;font-size:1.25em;margin:0.5em 0">Error 404:&nbsp;</h1>
     <h2 style="float:left;font-size:1.25em;margin:0.5em 0">not found!</h2>
     <p style="clear:left">The content you are looking for was not found. I'm sorry. <br>Maybe it has been moved or deleted. Who knows... not me for sure. <br>Better luck next time, slughead!</p>
-    <a href="./">Home</a>
+    <a href="index.php">Home</a>
 </section>
