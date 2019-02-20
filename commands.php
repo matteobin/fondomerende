9ef22bbe3a5dc2f4d5a466afa20b41b2aeeacc3b
matteobin <matteobin@protonmail.com>
2019-02-20 12:55:31 +0100

    Fixed big bug in eaten table.

--- commands.php
+++ commands.php
@@ -580,7 +580,7 @@ function eat($userId, $snackId, $quantity) {
         if (isset($outflowId)) {
             $dbManager->runPreparedQuery('UPDATE crates SET snack_quantity = snack_quantity-? WHERE outflow_id=?', array($quantity, $outflowId), 'ii');
             $dbManager->runPreparedQuery('UPDATE snacks_stock SET quantity = quantity-? WHERE snack_id=?', array($quantity, $snackId), 'ii');
-            $dbManager->runPreparedQuery('UPDATE eaten SET quantity = quantity+? WHERE snack_id=?', array($quantity, $snackId), 'ii');
+            $dbManager->runPreparedQuery('UPDATE eaten SET quantity = quantity+? WHERE user_id=? AND snack_id=?', array($quantity, $userId, $snackId), 'iii');
             $dbManager->runPreparedQuery('UPDATE users_funds SET amount = amount-? WHERE user_id=?', array($totalPrice, $userId), 'di');
             $dbManager->runPreparedQuery('INSERT INTO actions (user_id, command_id, snack_id, snack_quantity) VALUES (?, ?, ?, ?)', array($userId, 7, $snackId, $quantity), 'iiii');
             $response['response'] = array('success'=>true, 'status'=>200);
