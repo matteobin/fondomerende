ae088f01645915b5f24981c59576f1f3e915d54b
matteobin <matteobin@protonmail.com>
2019-05-22 15:02:20 +0200

    Finished apcu cache implementation in translation system.

--- translation.php
+++ translation.php
@@ -13,12 +13,15 @@ function getTranslatedString($fileName, $rowNumber) {
         $_SESSION['user-lang'] = $lang;
     }
     $rowIndex = $rowNumber-1;
-    $cacheKey = $_SESSION['user-lang'].'_lang_'.$fileName;
-    if (APCU_CACHE_INSTALLED && apcu_exists($cacheKey)) {
-        $cachedTranslatedRows = apcu_fetch($cacheKey);
+    $rowsCacheKey = $_SESSION['user-lang'].$fileName.'translation-rows';
+    $rowsGlobalVariableName = $_SESSION['user-lang'].str_replace('-', '', ucwords($fileName, '-')).'TranslationRows';
+    global $$rowsGlobalVariableName;
+    if (isset($$rowsGlobalVariableName[$rowIndex])) {
+        $translatedString = $$rowsGlobalVariableName[$rowIndex];
     }
-    if (APCU_CACHE_INSTALLED && isset($cachedTranslatedRows[$rowIndex])) {
+    else if (APCU_CACHE_INSTALLED && apcu_exists($rowsCacheKey) && isset(($cachedTranslatedRows = apcu_fetch($rowsCacheKey))[$rowIndex])) {
         $translatedString = $cachedTranslatedRows[$rowIndex];
+        $$rowsGlobalVariableName[$rowIndex] = $translatedString;
     } else {
         $filePath = '../lang/'.$lang.'/'.$fileName.'.txt';
         if (!is_file($filePath)) {
@@ -29,10 +32,11 @@ function getTranslatedString($fileName, $rowNumber) {
             }
         }
         if (!isset($translatedString)) {
-        $translationRows = file($filePath, FILE_IGNORE_NEW_LINES);
-        if (APCU_CACHE_INSTALLED) {
-            apcu_add($cacheKey, $translationRows);
-        }
+            $translationRows = file($filePath, FILE_IGNORE_NEW_LINES);
+            if (APCU_CACHE_INSTALLED) {
+                apcu_add($rowsCacheKey, $translationRows);
+            }
+            $$rowsGlobalVariableName = $translationRows;
             if ($rowNumber<=0 || $rowNumber>count($translationRows)) {
                 $translatedString = 'Invalid translation row number: there is no row number '.$rowNumber.' in '.$lang.' '.$fileName.' lang file.';
             } else {
