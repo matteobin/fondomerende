80f83601e7ca7d74f97feb7b8945798bcb1e397e
matteobin <matteobin@protonmail.com>
2019-08-09 10:00:44 +0200

    Improved UI.

--- views/404.php
+++ views/404.php
@@ -1,5 +1,5 @@
-    <h2 style="float:left;font-size:1.25em;margin:0.5em 0"><?php echoTranslatedString('404', 3); ?> <?php echoTranslatedString('404', 1); ?>:&nbsp;</h1>
+    <h2 class="one-column-row"><?php echoTranslatedString('404', 3); ?> <?php echoTranslatedString('404', 1); ?>:&nbsp;</h1>
 </header>
-<h3 style="float:left;font-size:1.25em;margin:0.5em 0"><?php echoTranslatedString('404', 2); ?></h2>
-<p style="clear:left"><?php echoTranslatedString('404', 4); ?> <br><?php echoTranslatedString('404', 5); ?> <br><?php echoTranslatedString('404', 6); ?></p>
-<?php echoTranslatedString('404', 7); ?> <a href="<?php echo BASE_DIR; ?>"><?php echoTranslatedString('404', 8); ?></a>, <?php echo $_SESSION['user-friendly-name']; ?>, <?php echoTranslatedString('404', 9); ?>.
+<h3 class="one-column-row"><?php echoTranslatedString('404', 2); ?></h2>
+<p class="one-column-row"><?php echoTranslatedString('404', 4); ?> <br><?php echoTranslatedString('404', 5); ?> <br><?php echoTranslatedString('404', 6); ?></p>
+<p class="one-column-row"><?php echoTranslatedString('404', 7); ?> <a href="<?php echo BASE_DIR; ?>"><?php echoTranslatedString('404', 8); ?></a>, <?php echo $_SESSION['user-friendly-name']; ?>, <?php echoTranslatedString('404', 9); ?>.</p>
--- views/maintenance.php
+++ views/maintenance.php
@@ -1,5 +1,5 @@
-    <h2><?php echoTranslatedString('maintenance', 3); ?></h2>
+    <h2 class="one-column-row"><?php echoTranslatedString('maintenance', 3); ?></h2>
 </header>
-<h3><?php echoTranslatedString('maintenance', 4); ?></h3>
-<p><?php echoTranslatedString('maintenance', 5); ?> <br><?php echoTranslatedString('maintenance', 6); ?> <br><?php echoTranslatedString('maintenance', 7); ?> <strong><?php echoTranslatedString('maintenance', 8); ?></strong>!<br><?php echoTranslatedString('maintenance', 9); ?></p>
-<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/D46a3jtH_cA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h3 class="one-column-row"><?php echoTranslatedString('maintenance', 4); ?></h3>
+<p class="one-column-row"><?php echoTranslatedString('maintenance', 5); ?> <br><?php echoTranslatedString('maintenance', 6); ?> <br><?php echoTranslatedString('maintenance', 7); ?> <strong><?php echoTranslatedString('maintenance', 8); ?></strong>!<br><?php echoTranslatedString('maintenance', 9); ?></p>
+<iframe class="one-column-row" src="https://www.youtube-nocookie.com/embed/D46a3jtH_cA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" width="560" height="315" allowfullscreen></iframe>
