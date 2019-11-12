630b1c83f1df18f941ae55c824b931b21862e78f
matteobin <matteobin@protonmail.com>
2019-11-12 14:54:06 +0100

    Added get snack image API to Postman and tested it.

--- commands/get-snack-image.php
+++ commands/get-snack-image.php
@@ -3,7 +3,7 @@ function downloadImageFromGoogle($name) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://www.google.it/search?q='.str_replace('-', '+', $name).'&tbm=isch&tbs=isz:i');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    if ($result = curl_exec($ch)!==false) {
+    if (($result = curl_exec($ch))!==false) {
         $needle = 'https://encrypted-tbn0.gstatic.com/images?q=';
         $needleLength = strlen($needle);
         $offset = 0;
@@ -14,6 +14,7 @@ function downloadImageFromGoogle($name) {
         }
         if (!empty($images)) {
             if (!file_exists(SNACK_IMAGES_PATH_FROM_PUBLIC_DIR)) {
+                echo 'ora';
                 mkdir(SNACK_IMAGES_PATH_FROM_PUBLIC_DIR, 0744, true);
             }
             file_put_contents(SNACK_IMAGES_PATH_FROM_PUBLIC_DIR.$name.'.'.IMAGES_EXTENSION, file_get_contents($images[rand(0, count($images)-1)]));        
@@ -24,7 +25,7 @@ function downloadImageFromGoogle($name) {
 function getSnackImage($name, $overwrite) {
     try {
         $path = SNACK_IMAGES_PATH_FROM_PUBLIC_DIR.$name.'.'.IMAGES_EXTENSION;
-        if ($overwrite || !file_exists($path)) {
+        if (true && ($overwrite || !file_exists($path))) {
             downloadImageFromGoogle($name);
         }
         if (APCU_INSTALLED) {
