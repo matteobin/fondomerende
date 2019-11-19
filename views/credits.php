6291bd396a6154c25874ed35b3c36e16e1f0dbcb
matteobin <matteobin@protonmail.com>
2019-11-19 09:26:56 +0100

    Removed credits.

--- views/credits.php
+++ views/credits.php
@@ -3,5 +3,3 @@
 <h3 class="one-column-row">v1.2.1b</h3>
 <p class="one-column-row"><?php echoTranslatedString('credits', 3); ?></p>
 <p class="one-column-row"><?php echoTranslatedString('credits', 4); ?></p>
-<p class="one-column-row"><?php echoTranslatedString('credits', 5); ?></p>
-<p class="one-column-row"><?php echoTranslatedString('credits', 6); ?></p>
