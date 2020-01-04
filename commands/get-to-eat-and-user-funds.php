1122a2406f22b04fb5e5da8e34ff2e17e5e006e5
matteobin <matteobin@protonmail.com>
2020-01-04 18:26:08 +0100

    Removed 404 snack id eat bug and optimised get snacks to eat query.

--- commands/get-to-eat-and-user-funds.php
+++ commands/get-to-eat-and-user-funds.php
@@ -7,19 +7,14 @@ function getToEatAndUserFunds($userId) {
         $dbManager->runQuery('LOCK TABLES users_funds READ, snacks_stock READ, crates READ, snacks READ');
 		$userFundsAmount = getUserFunds($userId, false);
 		$snacks = array();
-        $dbManager->runPreparedQuery('SELECT snacks_stock.snack_id, snacks_stock.quantity, (SELECT crates.expiration FROM crates WHERE crates.snack_id=snacks_stock.snack_id AND crates.snack_quantity!=? ORDER BY crates.expiration ASC LIMIT 1) as expiration FROM snacks_stock WHERE snacks_stock.quantity!=? ORDER BY expiration ASC', array(0, 0), 'ii');
+        $dbManager->runPreparedQuery('SELECT snacks_stock.snack_id, snacks.friendly_name, snacks_stock.quantity, (SELECT crates.expiration FROM crates WHERE crates.snack_id=snacks_stock.snack_id AND crates.snack_quantity!=? ORDER BY crates.expiration ASC LIMIT 1) as expiration FROM snacks_stock JOIN snacks ON snacks_stock.snack_id=snacks.id WHERE snacks_stock.quantity!=? ORDER BY expiration ASC', array(0, 0), 'ii');
         while ($snacksStockRow = $dbManager->getQueryRes()->fetch_assoc()) {
-			$snacks[] = array('id'=>$snacksStockRow['snack_id'], 'quantity'=>$snacksStockRow['quantity'], 'expiration'=>$snacksStockRow['expiration']);
+			$snacks[] = array('id'=>$snacksStockRow['snack_id'], 'friendly-name'=>$snacksStockRow['friendly_name'], 'quantity'=>$snacksStockRow['quantity'], 'expiration'=>$snacksStockRow['expiration']);
 		}
 		foreach ($snacks as &$snack) {
 			$dbManager->runPreparedQuery('SELECT price_per_snack FROM crates WHERE snack_id=? AND snack_quantity!=? ORDER BY expiration ASC LIMIT 1', array($snack['id'], 0), 'ii');
 			while ($cratesRow = $dbManager->getQueryRes()->fetch_assoc()) {
 				$snack['price-per-snack'] = $cratesRow['price_per_snack'];
-			}
-			$dbManager->runPreparedQuery('SELECT name, friendly_name FROM snacks WHERE id=?', array($snack['id']), 'i');
-			while ($snacksRow = $dbManager->getQueryRes()->fetch_assoc()) {
-				$snack['name'] = $snacksRow['name'];
-				$snack['friendly-name'] = $snacksRow['friendly_name'];
 			}
         }
         $dbManager->runQuery('UNLOCK TABLES');
